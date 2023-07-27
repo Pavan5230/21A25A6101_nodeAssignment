62a47bba598d2c77d06caebed84ced924db989ed
Pavan5230 <104582622+Pavan5230@users.noreply.github.com>
2023-07-27 13:37:02 +0530

    Add files via upload

--- task3.js
+++ task3.js
@@ -5,14 +5,14 @@ const transporter = nodemailer.createTransport({
   service: 'GMAIL',
   auth: {
     user: 'pavandemo19@gmail.com',
-    pass: '21a25a6101'
+    pass: 'lecepugpxekxqmso'
   }
 });
 
 
 const mailOptions = {
   from: 'pavandemo19@gmail.com', 
-  to: 'pravin.consensus@gmail.com',
+  to: 'pavanadabala8@gmail.com',
   subject: '21A25A6101',
   text: 'This is 21a25a6101(A.pavan)  sending email'
 };
